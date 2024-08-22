2d0c0b83ad7aa0aee3dc992e39586b55b3115e0d
Omkar Og <157821973+omkarOG1@users.noreply.github.com>
2024-08-22 17:36:07 +0000

    Arrays

---
+++ javascript_01/Array.js
@@ -0,0 +1,10 @@
+// console.log(Array.isArray("Omkar"))
+// console.log(Array.from("Omkar")) //make a  array 
+// console.log(Array.from({name:"omkar"}))//gives an empty array
+
+let score1 =100;
+let score2=200;
+let score3=300;
+
+console.log(Array.of(score1,score2,score3)) //converts into an array 
+
